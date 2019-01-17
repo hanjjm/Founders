1a2005e2262e37a4d0a886b83e10c083050a8024
hanjjm <hanjjm1994@naver.com>
2019-01-17 18:25:34 +0900

    add review system

--- assets/js/review.js
+++ assets/js/review.js
@@ -2,3 +2,13 @@ function review(){
   alert("리뷰가 등록되었습니다.")
   location.href="review.html"
 }
+
+function like(){
+  alert("좋아요를 누르셨습니다. 당신의 좋아요는 리뷰작성자에게 큰 힘이 됩니다.")
+  location.href="review.html"
+}
+
+function dislike(){
+  alert("싫어요를 누르셨습니다. 당신의 싫어요는 리뷰작성자를 실망하게 만듭니다.")
+  location.href="review.html"
+}
